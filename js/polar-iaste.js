63f7d7e684def283346c7ffb6c264a722e84abe6
matthewenglish <matthew.english@lumary.com.au>
2023-02-10 14:59:28 +1030

    Update polar-iaste.js

--- js/polar-iaste.js
+++ js/polar-iaste.js
@@ -3,9 +3,9 @@
 ********************/
 function initTextZone() {
   $('#generate-text').click(function () {
-    var option, data;
-    data = JSON.parse($('#text-area').val())
-    option = $('#text-select').val()
+    var selfData, peerData;
+    selfData = JSON.parse($('#text-area-self').val())
+	peerData = JSON.parse($('#text-area-peer').val())    
     margin = 100;
     config = {
       scaleShowLabels: false,
@@ -15,29 +15,8 @@ function initTextZone() {
       scaleSteps: 10,
       scaleStepWidth: 10,
       scaleStartValue: 0
-    };
-    switch(option) {
-      case 'Custom':
-        margin = 0;
-        config.scaleShowLine = false;
-        config.showLabels = false;
-        break;
-      case '4x2':
-      case '4x3':
-        config.scaleShowXYAxis = true;
-        break;
-      case '5x2':
-        config.scaleShowQuintAxis = true;
-        break;
-      case '2x*':
-        config.scaleShowXAxis = true;
-        config.startAngle = -Math.PI;
-        config.sectionMargin = 25;
-        break;
-      default:
-        break;
-    }
-    displayChart(data, config, margin)
+    };    
+    displayChart(selfData, config, margin)
   });
 }
 
@@ -58,7 +37,7 @@ function displayChart(data, config, margin) {
   }
   console.log(config)
   new Chart($("#canvas").get(0).getContext("2d"), margin).PolarArea(data, config);
-  $('#text-area').val(JSON.stringify(data, null, '\t'));
+  //$('#text-area').val(JSON.stringify(data, null, '\t'));
 }
 
 function initColorPickers() {
